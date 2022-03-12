8272760d0ebb9e18592e9b811f915397b8a966c3
hatter <zaeem.z@rutgers.edu>
2022-03-12 15:02:56 -0500

    final update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# historyLink" 
+
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/hattvr/historyLink/master/historykey.ps1'));
--- historykey.ps1
+++ historykey.ps1
@@ -1,58 +1,60 @@
-function Show-Error{
-    param ($errorItem)
-    Write-Host "Sorry, we cannot find $errorItem!" -ForegroundColor Red
-    Write-Host "Make sure to open the wish history ingame first." -ForegroundColor Yellow
-    Write-Host "If you keep seeing this issue, try to run powershell as administrator." -ForegroundColor Yellow
+function error{
+    param ($exception)
+    Write-Host "There's been an excpetion! We cannot find $exception!" -ForegroundColor Red
+    Write-Host "Make sure you have opened the wish history ingame before trying to grab the link!" -ForegroundColor DarkRed
+    Write-Host "Run powershell as administrator if this error persists." -ForegroundColor DarkRed
 }
-function Get-Link{
+function getLink{
     param ($pathToFile)
     $logs = Get-Content -Path $pathToFile
     $match = $logs -match "^OnGetWebViewPageFinish.*log$"
     if (-Not $match) {
-        Show-Error -errorItem "WISH HISTORY URL" 
+        error -exception "wish " 
         return
     }
     [string] $wishHistoryUrl = $match[$match.count-1] -replace 'OnGetWebViewPageFinish:', ''
     Set-Clipboard -Value $wishHistoryUrl
     Write-Host "Link has successfully been copied to your clipboard!" -ForegroundColor Green
-    Write-Host "That's it! Paste the link copied to the required field in our discord." -ForegroundColor Cyan
+    Write-Host "That's it! Paste the link to the required field in our discord." -ForegroundColor Cyan
 }
 
-Write-Host "███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████" -ForegroundColor Magenta
-Write-Host "█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░░░░░█░░░░░░██████████░░░░░░████░░░░░░██████████░░░░░░█░░░░░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░███░░░░░░░░░░░░███" -ForegroundColor Magenta
-Write-Host "█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░░░░░░░░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀░░░░░░░░░░██░░▄▀░░████░░▄▀░░██████████░░▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀▄▀░░░░█" -ForegroundColor Magenta
-Write-Host "█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░░░█░░▄▀▄▀▄▀▄▀▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀░░█░░░░▄▀░░░░█░░▄▀▄▀▄▀▄▀▄▀░░██░░▄▀░░████░░▄▀░░██████████░░▄▀░░█░░░░▄▀░░░░█░░░░░░░░░░░░▄▀▄▀░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░▄▀░░███░░▄▀░░░░▄▀▄▀░░█" -ForegroundColor Magenta
-Write-Host "█░░▄▀░░█████████░░▄▀░░█████████░░▄▀░░░░░░▄▀░░██░░▄▀░░█░░▄▀░░█████████░░▄▀░░██░░▄▀░░███░░▄▀░░███░░▄▀░░░░░░▄▀░░██░░▄▀░░████░░▄▀░░██████████░░▄▀░░███░░▄▀░░███████████░░░░▄▀░░░░█░░▄▀░░██░░▄▀░░█░░▄▀░░████░░▄▀░░███░░▄▀░░██░░▄▀░░█" -ForegroundColor Magenta
-Write-Host "█░░▄▀░░█████████░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░███░░▄▀░░███░░▄▀░░██░░▄▀░░██░░▄▀░░████░░▄▀░░██░░░░░░██░░▄▀░░███░░▄▀░░█████████░░░░▄▀░░░░███░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░▄▀░░███░░▄▀░░██░░▄▀░░█" -ForegroundColor Magenta
-Write-Host "█░░▄▀░░██░░░░░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░███░░▄▀░░███░░▄▀░░██░░▄▀░░██░░▄▀░░████░░▄▀░░██░░▄▀░░██░░▄▀░░███░░▄▀░░███████░░░░▄▀░░░░█████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀░░██░░▄▀░░█" -ForegroundColor Magenta
-Write-Host "█░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░░░░░░░░░▄▀░░█░░▄▀░░░░░░▄▀░░███░░▄▀░░███░░▄▀░░██░░▄▀░░██░░▄▀░░████░░▄▀░░██░░▄▀░░██░░▄▀░░███░░▄▀░░█████░░░░▄▀░░░░███████░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░▄▀░░░░███░░▄▀░░██░░▄▀░░█" -ForegroundColor Magenta
-Write-Host "█░░▄▀░░██░░▄▀░░█░░▄▀░░█████████░░▄▀░░██░░▄▀░░░░░░▄▀░░█████████░░▄▀░░█░░▄▀░░██░░▄▀░░███░░▄▀░░███░░▄▀░░██░░▄▀░░░░░░▄▀░░████░░▄▀░░░░░░▄▀░░░░░░▄▀░░███░░▄▀░░███░░░░▄▀░░░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█████░░▄▀░░██░░▄▀░░█" -ForegroundColor Magenta
-Write-Host "█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀▄▀▄▀▄▀▄▀░░█░░░░░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░█░░░░▄▀░░░░█░░▄▀░░██░░▄▀▄▀▄▀▄▀▄▀░░████░░▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀░░█░░░░▄▀░░░░█░░▄▀▄▀░░░░░░░░░░░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░░░░░█░░▄▀░░░░▄▀▄▀░░█" -ForegroundColor Magenta
-Write-Host "█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░░░░░░░░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀░░██░░░░░░░░░░▄▀░░████░░▄▀░░░░░░▄▀░░░░░░▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀░░░░█" -ForegroundColor Magenta
-Write-Host "█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░░░░░█░░░░░░██████████░░░░░░████░░░░░░██░░░░░░██░░░░░░█░░░░░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░██░░░░░░░░░░█░░░░░░░░░░░░███" -ForegroundColor Magenta
-Write-Host "███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████" -ForegroundColor Magenta
+$primaryPath  = [System.Environment]::ExpandEnvironmentVariables("%userprofile%\AppData\LocalLow\miHoYo\Genshin Impact\output_log.txt");
+$secondaryPath = [System.Environment]::ExpandEnvironmentVariables("%userprofile%\AppData\LocalLow\miHoYo\$([char]0x539f)$([char]0x795e)\output_log.txt");
+$boolPrimary = [System.IO.File]::Exists($primaryPath);
+$boolSecondary = [System.IO.File]::Exists($secondaryPath);
+
+
+Write-Host "" -ForegroundColor Magenta                                                                                                                                                                                           
+Write-Host "      _____         ______    _____   ______            ______   ____   ____  ____  _____   ______           _____            ____    _____                ____        _____        _____   " -ForegroundColor Cyan 
+Write-Host "  ___|\    \    ___|\     \  |\    \ |\     \       ___|\     \ |    | |    ||    ||\    \ |\     \         |\    \   _____  |    |  /    /|___       ____|\   \   ___|\    \   ___|\    \  " -ForegroundColor Cyan 
+Write-Host " /    /\    \  |     \     \  \\    \| \     \     |    |\     \|    | |    ||    | \\    \| \     \        | |    | /    /| |    | /    /|    |     /    /\    \ |    |\    \ |    |\    \ " -ForegroundColor Cyan 
+Write-Host "|    |  |____| |     ,_____/|  \|    \  \     |    |    |/____/||    |_|    ||    |  \|    \  \     |       \/     / |    || |    ||\____\|    |    |    |  |    ||    | |    ||    | |    |" -ForegroundColor Cyan 
+Write-Host "|    |    ____ |     \--'\_|/   |     \  |    | ___|    \|   | ||    .-.    ||    |   |     \  |    |       /     /_  \   \/ |    || |   |/    |___ |    |__|    ||    |/____/ |    | |    |" -ForegroundColor Cyan 
+Write-Host "|    |   |    ||     /___/|     |      \ |    ||    \    \___|/ |    | |    ||    |   |      \ |    |      |     // \  \   \ |    | \|___/    /    ||    .--.    ||    |\    \ |    | |    |" -ForegroundColor Cyan 
+Write-Host "|    |   |_,  ||     \____|\    |    |\ \|    ||    |\     \    |    | |    ||    |   |    |\ \|    |      |    |/   \ |    ||    |    /     /|    ||    |  |    ||    | |    ||    | |    |" -ForegroundColor Cyan 
+Write-Host "|\ ___\___/  /||____ '     /|   |____||\_____/||\ ___\|_____|   |____| |____||____|   |____||\_____/|      |\ ___/\   \|   /||____|   |_____|/____/||____|  |____||____| |____||____|/____/|" -ForegroundColor Cyan 
+Write-Host "| |   /____ / ||    /_____/ |   |    |/ \|   ||| |    |     |   |    | |    ||    |   |    |/ \|   ||      | |   | \______/ ||    |   |     |    | ||    |  |    ||    | |    ||    /    | |" -ForegroundColor Cyan 
+Write-Host " \|___|    | / |____|     | /   |____|   |___|/ \|____|_____|   |____| |____||____|   |____|   |___|/       \|___|/\ |    | ||____|   |_____|____|/ |____|  |____||____| |____||____|____|/ " -ForegroundColor Cyan 
+Write-Host "   \( |____|/    \( |_____|/      \(       )/      \(    )/       \(     )/    \(       \(       )/            \(   \|____|/   \(       \(    )/      \(      )/    \(     )/    \(    )/   " -ForegroundColor Cyan 
+Write-Host "    '   )/        '    )/          '       '        '    '         '     '      '        '       '              '      )/       '        '    '        '      '      '     '      '    '    " -ForegroundColor Cyan 
+Write-Host "       '              '                                                                                               '                   " -ForegroundColor Cyan                                                  
 Write-Host ""
 
-$globalPath  = [System.Environment]::ExpandEnvironmentVariables("%userprofile%\AppData\LocalLow\miHoYo\Genshin Impact\output_log.txt");
-$chinaPath = [System.Environment]::ExpandEnvironmentVariables("%userprofile%\AppData\LocalLow\miHoYo\$([char]0x539f)$([char]0x795e)\output_log.txt");
-$globalPathExist = [System.IO.File]::Exists($globalPath);
-$chinaPathExist = [System.IO.File]::Exists($chinaPath);
-
-if ($globalPathExist -xor $chinaPathExist){
-    if($globalPathExist){
-        Get-Link -pathToFile $globalPath
+if ($boolPrimary -xor $boolSecondary){
+    if($boolPrimary){
+        getLink -pathToFile $primaryPath
     } else {
-        Get-Link -pathToFile $chinaPath
+        getLink -pathToFile $secondaryPath
     }
 } else {
-    if ($globalPathExist -and $chinaPathExist){     
-        if(((Get-ItemProperty -Path $chinaPath -Name LastWriteTime).lastwritetime - (Get-ItemProperty -Path $globalPath -Name LastWriteTime).lastwritetime) -gt 0){
-            Get-Link -pathToFile $chinaPath
+    if ($boolPrimary -and $boolSecondary){     
+        if(((Get-ItemProperty -Path $secondaryPath -Name LastWriteTime).lastwritetime - (Get-ItemProperty -Path $primaryPath -Name LastWriteTime).lastwritetime) -gt 0){
+            getLink -pathToFile $secondaryPath
         } else {
-            Get-Link -pathToFile $globalPath
+            getLink -pathToFile $primaryPath
         }
     } else {
-        Show-Error -errorItem "GENSHIN IMPACT LOG FILE"
+        error -exception "GENSHIN IMPACT LOG FILE"
         return
     }
 }
