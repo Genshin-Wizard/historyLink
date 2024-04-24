db4e2e2eb4162f694b0d9f19f285bb43dcc57ad4
hatter <hatterhg@gmail.com>
2024-04-24 19:43:42 -0400

    test new script

--- historykey.ps1
+++ historykey.ps1
@@ -28,113 +28,111 @@ Write-Host "              '      )/       '        '    '        '      '      '
 Write-Host "                     '                                                                                       " -ForegroundColor Red                                                     
 Write-Host "" -ForegroundColor Magenta     
 
+function processWishUrl($wishUrl) {
+    if ($wishUrl -match "https:\/\/webstatic") {
+        if ($wishUrl -match "hk4e_global") {
+            $checkUrl = $wishUrl -replace "https:\/\/webstatic.+html\?", "https://hk4e-api-os.mihoyo.com/event/gacha_info/api/getGachaLog?"
+        } else {
+            $checkUrl = $wishUrl -replace "https:\/\/webstatic.+html\?", "https://hk4e-api.mihoyo.com/event/gacha_info/api/getGachaLog?"
+        }
+        $urlResponseMessage = Invoke-RestMethod -URI $checkUrl | % {$_.message}
+    } else {
+        $urlResponseMessage = Invoke-RestMethod -URI $wishUrl | % {$_.message}
+    }
 
-$logLocation = "%userprofile%\AppData\LocalLow\miHoYo\Genshin Impact\output_log.txt";
-$logLocationChina = "%userprofile%\AppData\LocalLow\miHoYo\$([char]0x539f)$([char]0x795e)\output_log.txt";
+    if ($urlResponseMessage -ne "OK") {
+        Write-Host "There's been an exception! Link has been found but is expired/invalid!" -ForegroundColor Yellow
+        Write-Host "Open the wish history ingame before trying again to fetch a new link!" -ForegroundColor Cyan
+        return $False
+    }
 
-$reg = $args[0]
-$apiHost = "hk4e-api-os.hoyoverse.com" 
-if ($reg -eq "china") {
-  Write-Host "[INFO] Using China cache log location..." -ForegroundColor Cyan
-  $logLocation = $logLocationChina
-  $apiHost = "hk4e-api.mihoyo.com"
+    Write-Host $wishURL
+    Set-Clipboard -Value $wishURL
+    
+    Write-Host "Link has successfully been copied to your clipboard!" -ForegroundColor Green
+    Write-Host "That's it! Paste the link to the required field in our discord." -ForegroundColor Cyan
+    
+    return $True
 }
 
-$tmps = $env:TEMP + '\pm.ps1';
-if ([System.IO.File]::Exists($tmps)) {
-    Remove-Item $tmps;
+$logLocation = [System.Environment]::ExpandEnvironmentVariables("%userprofile%/AppData/LocalLow/miHoYo/Genshin Impact/output_log.txt");
+$logLocationChina = [System.Environment]::ExpandEnvironmentVariables("%userprofile%/AppData/LocalLow/miHoYo/$([char]0x539f)$([char]0x795e)/output_log.txt");
+$globalExists = Test-Path $logPathGlobal;
+$cnExists = Test-Path $logPathChina;
+
+if ($globalExists) {
+    if ($cnExists) {
+        if ((Get-Item $logPathGlobal).LastWriteTime -ge (Get-Item $logPathChina).LastWriteTime) {
+            $logPath = $logPathGlobal;
+        } else {
+            $logPath = $logPathChina;
+        }
+    } else {
+        $logPath = $logPathGlobal;
+    } 
+} else {
+    if ($cnExists) {
+        $logPath = $logPathChina;
+    } else {
+        Write-Host "There's been an exception! We cannot find the wish history url!" -ForegroundColor Red
+        Write-Host "Make sure you have opened the wish history ingame before trying to grab the link!" -ForegroundColor Cyan
+        if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {  
+            Write-Host "Do you want to try to run the script as Administrator? Press [ENTER] to continue, or any key to cancel."
+            $keyInput = [Console]::ReadKey($true).Key
+            if ($keyInput -ne "13") {
+                return
+            }
+
+            $arguments = "& '" +$myinvocation.mycommand.definition + "'"
+            Start-Process powershell -Verb runAs -ArgumentList "-noexit $arguments $reg"
+            break
+        } 
+        return
+    }
 }
 
-$path = [System.Environment]::ExpandEnvironmentVariables($logLocation);
-if (-Not [System.IO.File]::Exists($path)) {
+$logs = Get-Content -Path $logPath
+$regexPattern = "(?m).:/.+(GenshinImpact_Data|YuanShen_Data)"
+$logMatch = $logs -match $regexPattern
+
+if (-Not $logMatch) {
     Write-Host "There's been an exception! We cannot find the wish history url!" -ForegroundColor Red
     Write-Host "Make sure you have opened the wish history ingame before trying to grab the link!" -ForegroundColor Cyan
+    pause
+    return
+}
 
-    if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {  
-        Write-Host "Do you want to try to run the script as Administrator? Press [ENTER] to continue, or any key to cancel." -ForegroundColor Yellow
-        $keyInput = [Console]::ReadKey($true).Key
-        if ($keyInput -ne "13") {
+$gameDataPath = ($logMatch | Select -Last 1) -match $regexPattern
+$gameDataPath = Resolve-Path $Matches[0]
+
+$webcachePath = Resolve-Path "$gameDataPath/webCaches"
+$cacheVerPath = Get-Item (Get-ChildItem -Path $webcachePath | Sort-Object LastWriteTime -Descending | Select-Object -First 1).FullName
+$cachePath = Resolve-Path "$cacheVerPath/Cache/Cache_Data/data_2"
+
+if (Test-Path $cachePath) {
+    $tmpFile = "$env:TEMP/ch_data_2"
+    Copy-Item $cachePath -Destination $tmpFile
+    $content = Get-Content -Encoding UTF8 -Raw $tmpfile
+    $splitted = $content -split "1/0/" | Select -Last 1
+    $found = $splitted -match "https.+?game_biz=hk4e_(global|cn)"
+    Remove-Item $tmpFile
+    
+    if ($found) {
+        $wishUrl = $Matches[0]
+        if (processWishUrl $wishUrl) {
             return
         }
-
-        $myinvocation.mycommand.definition > $tmps
-
-        Start-Process powershell -Verb runAs -ArgumentList "-noexit", $tmps, $reg
-        break
     }
 
-    return
-}
-
-$logs = Get-Content -Path $path
-$m = $logs -match "(?m).:/.+(GenshinImpact_Data|YuanShen_Data)"
-$m[0] -match "(.:/.+(GenshinImpact_Data|YuanShen_Data))" >$null
-
-if ($matches.Length -eq 0) {
-    Write-Host "There's been an exception! We cannot find the wish history url!" -ForegroundColor Red
-    Write-Host "Make sure you have opened the wish history ingame before trying to grab the link!" -ForegroundColor Cyan
-    return
-}
-
-$gamedir = $matches[1]
-$webcachePath = Resolve-Path "$gamedir/webCaches"
-$cacheVerPath = Get-Item (Get-ChildItem -Path $webcachePath | Sort-Object LastWriteTime -Descending | Select-Object -First 1).FullName
-$cachefile = Resolve-Path "$cacheVerPath/Cache/Cache_Data/data_2"
-$tmpfile = "$env:TEMP/ch_data_2"
-
-Copy-Item $cachefile -Destination $tmpfile
-
-function testUrl($url) {
-  $ProgressPreference = 'SilentlyContinue'
-  $uri = [System.UriBuilder]::New($url)
-  $uri.Path = "event/gacha_info/api/getGachaLog"
-  $uri.Host = $apiHost
-  $uri.Fragment = ""
-  $params = [System.Web.HttpUtility]::ParseQueryString($uri.Query)
-  $params.Set("lang", "en");
-  $params.Set("gacha_type", 301);
-  $params.Set("size", "5");
-  $params.Add("lang", "en-us");
-  $uri.Query = $params.ToString()
-  $apiUrl = $uri.Uri.AbsoluteUri
-
-  $response = Invoke-WebRequest -Uri $apiUrl -ContentType "application/json" -UseBasicParsing -TimeoutSec 10 | ConvertFrom-Json
-  $testResult = $response.retcode -eq 0
-  return $testResult
-}
-
-$content = Get-Content -Encoding UTF8 -Raw $tmpfile
-$splitted = $content -split "1/0/"
-$found = $splitted -match "webview_gacha"
-$link = $linkFound = $false
-
-for ($i = $found.Length - 1; $i -ge 0; $i -= 1) {
-    $t = $found[$i] -match "(https.+?game_biz=)"
-    $link = $matches[0]
-    Write-Host "`rChecking Link $i" -NoNewline
-    $testResult = testUrl $link
-    if ($testResult -eq $true) {
-        $linkFound = $true
-        break
-    }
-    Sleep 1
-}
-
-Remove-Item $tmpfile
-
-Write-Host ""
-
-if (-Not $linkFound) {
     Write-Host "There's been an exception! We cannot find the wish history url!" -ForegroundColor Red
     Write-Host "Make sure you have opened the wish history ingame before trying to grab the link!" -ForegroundColor Cyan
     
-    return
+    pause
+} else {
+    Write-Host "There's been an exception! We cannot find the wish history url!" -ForegroundColor Red
+    Write-Host "Make sure you have opened the wish history ingame before trying to grab the link!" -ForegroundColor Cyan
+    
+    pause
 }
-
-$wishHistoryUrl = $link
-
-Set-Clipboard -Value $wishHistoryUrl
-Write-Host "Link has successfully been copied to your clipboard!" -ForegroundColor Green
-Write-Host "That's it! Paste the link to the required field in our discord." -ForegroundColor Cyan
 
 pause
